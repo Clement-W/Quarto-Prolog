606edec9b619072b488e2ebc475084e34f35f949
VictorBREHAULT <vbrehault@ensc.fr>
2022-03-11 01:33:51 +0100

    Ajout de commentaires dans IAMinMax

--- IAMinMax.pl
+++ IAMinMax.pl
@@ -1,19 +1,26 @@
 :- ['Utils.pl'].
 :- ['Pieces.pl'].
 
+% Les deux prédicats suivants sont appelés dans JouervsIADifficile dans Jeu.pl, nous avons décidé de fixer arbitrairement la profondeur de l'arbre d'exploration de l'algorithme à 4
 selectionnerPieceIADifficile(P, Plateau):- minMax(Plateau, P, 4).
 placerPieceIADifficile(P, Plateau, NouveauPlateau):- minMax(Plateau, P, IndPiece, 4), changerElemListe(IndPiece, P, Plateau, NouveauPlateau).
 
+% minMax enclenche l'exécution de l'algorithme de l'IA pour :
+    % sélectionner la pire pièce possible pour l'adversaire
 minMax(Plateau, PieceASelectionner, ProfondeurMax):- listePiecesRestantes(Plateau, ListePiecesRestantes),length(ListePiecesRestantes, NbPiecesRestantes), premierEtage(Plateau, ListePiecesRestantes, PieceASelectionner, 1, NbPiecesRestantes, ProfondeurMax, _).
+    % placer à la meilleur place la pièce choisie par l'adversaire
 minMax(Plateau, PieceAPlacer, IndPieceAJouer, ProfondeurMax):- listePiecesRestantes(Plateau, ListePiecesRestantes), subtract(ListePiecesRestantes, [PieceAPlacer], ListesPiecesRestantesSansLaPieceAPlacer), length(ListesPiecesRestantesSansLaPieceAPlacer, NbPiecesRestantes), premierEtage(Plateau, PieceAPlacer, IndPieceAJouer, _, NbPiecesRestantes, ProfondeurMax).
 
+% premierEtage construit le premier niveau de l'arbre d'exloration.
 % Cas où l'IA doit sélectionner une pièce pour son adversaire
+    % Cas d'arrêt : on arrive au bout du niveau et on remonte le score correspondant au choix de la dernière pièce possible à placer
 premierEtage(Plateau, ListePiecesRestantes, PieceATester, NbPiecesRestantes, NbPiecesRestantes, ProfondeurMax, ScorePiece):-
     nth1(NbPiecesRestantes, ListePiecesRestantes, PieceATester),
     listeCasesRestantes(Plateau, [], 1, ListeCasesRestantes),
     nth1(IndPiece, Plateau, PieceATester), 
     creationNoeudDuNiveau(Plateau, PieceATester, ScorePiece, 0, ListeCasesRestantes, 1, IndPiece, 1, ProfondeurMax, 1, NbPiecesRestantes).
 
+    % Cas où la pièce testée donne un score plus mauvais que le score enregistré, et donc on garde cette pièce et ce score
 premierEtage(Plateau, ListePiecesRestantes, PieceATester, IndPieceRestante, NbPiecesRestantes, ProfondeurMax, ScorePiece):-
     nth1(IndPieceRestante, ListePiecesRestantes, PieceATester),
     listeCasesRestantes(Plateau, [], 1, ListeCasesRestantes),
@@ -23,6 +30,7 @@ premierEtage(Plateau, ListePiecesRestantes, PieceATester, IndPieceRestante, NbPi
     premierEtage(Plateau, ListePiecesRestantes, _, 0, IndPieceRestanteSuivant, NbPiecesRestantes, ProfondeurMax, ScorePieceSuivante),
     ScorePieceSuivante > ScorePiece.
 
+    % Cas où la pièce testée donne un score meilleur que le score enregistré, et donc ne la considère pas
 premierEtage(Plateau, ListePiecesRestantes, PieceASelectionner, IndPieceRestante, NbPiecesRestantes, ProfondeurMax, ScorePieceSuivante):-
     nth1(IndPieceRestante, ListePiecesRestantes, PieceATester),
     listeCasesRestantes(Plateau, [], 1, ListeCasesRestantes), 
@@ -33,12 +41,14 @@ premierEtage(Plateau, ListePiecesRestantes, PieceASelectionner, IndPieceRestante
     ScorePieceSuivante < ScorePiece.
 
 %Cas où l'IA doit placer une pièce sélectionner par l'adversaire
+    % Cas d'arrêt : on arrive au bout du niveau et on remonte le score correspondant au choix de la dernière place possible pour la pièce à placer
 premierEtage(Plateau, PieceAPlacer, IndTeste, ScorePlace, 1, ProfondeurMax):-
     listeCasesRestantes(Plateau, [], 1, ListeCasesRestantes),
     nth1(1, ListeCasesRestantes, IndTeste),
     changerElemListe(IndTeste, PieceAPlacer, Plateau, PlateauAvecPiece),
     creationNoeudDuNiveau(PlateauAvecPiece, _, ScorePlace, 1, _, _, _, 1, ProfondeurMax, 1, 1).
 
+    % Cas où la place testée est moins bonne que la place enregistrée, donc on ne la considère pas
 premierEtage(Plateau, PieceAPlacer, IndPieceAPlacer, ScorePlaceSuivante, NbCasesNonTestees, ProfondeurMax):-
     listeCasesRestantes(Plateau, [], 1, ListeCasesRestantes),
     nth1(NbCasesNonTestees, ListeCasesRestantes, IndTeste),
@@ -48,6 +58,7 @@ premierEtage(Plateau, PieceAPlacer, IndPieceAPlacer, ScorePlaceSuivante, NbCases
     premierEtage(Plateau, PieceAPlacer, IndPieceAPlacer, ScorePlaceSuivante, 1, NbCasesNonTesteActualise, ProfondeurMax),
     ScorePlaceSuivante > ScorePlace.
 
+    % Cas où la place testée est meilleur que la place enregistrée, donc on l'enregistre
 premierEtage(Plateau, PieceAPlacer, IndTeste, ScorePlace, NbCasesNonTestees, ProfondeurMax):-
     listeCasesRestantes(Plateau, [], 1, ListeCasesRestantes),
     nth1(NbCasesNonTestees, ListeCasesRestantes, IndTeste),
@@ -57,12 +68,14 @@ premierEtage(Plateau, PieceAPlacer, IndTeste, ScorePlace, NbCasesNonTestees, Pro
     premierEtage(Plateau, PieceAPlacer, _, ScorePlaceSuivante, 1, NbCasesNonTesteActualise, ProfondeurMax),
     ScorePlaceSuivante < ScorePlace.
 
+% Évalue un état du plateau, en fonction du nombre de caractéristiques en commun des pièces alignées
 score(Plateau, Score) :-
     evaluerLignes(Plateau, ScoreLignes),
     evaluerColonnes(Plateau, ScoreColonnes),
     evaluerDiagonales(Plateau, ScoreDiagonales),
     Score is ScoreLignes+ScoreColonnes+ScoreDiagonales.
 
+% Donne le score du plateau en fonction des lignes uniquement
 evaluerLignes(Plateau, ScoreLignes) :-
     nth1(1, Plateau, E1),
     nth1(2, Plateau, E2),
@@ -86,6 +99,7 @@ evaluerLignes(Plateau, ScoreLignes) :-
     evaluation(E13, E14, E15, E16, Score4),
     ScoreLignes is Score1+Score2+Score3+Score4.
 
+% Donne le score du plateau en fonction des colonnes uniquement
 evaluerColonnes(Plateau, ScoreColonnes) :-
     nth1(1, Plateau, E1),
     nth1(5, Plateau, E5),
@@ -109,6 +123,7 @@ evaluerColonnes(Plateau, ScoreColonnes) :-
     evaluation(E4, E8, E12, E16, Score4),
     ScoreColonnes is Score1+Score2+Score3+Score4.
 
+% Donne le score du plateau en fonction des diagonales uniquement
 evaluerDiagonales(Plateau, ScoreDiagonales) :-
     nth1(1, Plateau, E1),
     nth1(6, Plateau, E6),
@@ -121,6 +136,8 @@ evaluerDiagonales(Plateau, ScoreDiagonales) :-
     nth1(13, Plateau, E13),
     evaluation(E4, E7, E10, E13, Score2),
     ScoreDiagonales is Score1+Score2.
+
+% evaluation est vrai pour chaque alignement de 4 pièces pouvant aboutir à la victoire, et donne un score selon la proximité du cas de victoire
 
 % Cas de victoire
 evaluation([X, _, _, _], [X, _, _, _], [X, _, _, _], [X, _, _, _], 1000).
@@ -181,6 +198,8 @@ evaluation(vide, vide, [_, _, _, X], [_, _, _, X], 2).
 % Cas où il est impossible de gagner avec l'alignement des 4 emplacements évalués
 evaluation(_, _, _, _, 0).
 
+
+%creationNoeudDuNiveau crée les noeuds suivant de l'arbre d'exploration à partir du noeud traité
 
 % Cas d'arrêt : profondeur maximale et fin de niveau
 creationNoeudDuNiveau(NoeudPlateau, _, ScoreNoeudPrecedent, _, _, _, _, ProfondeurMax, ProfondeurMax, NumeroMax, NumeroMax) :-
